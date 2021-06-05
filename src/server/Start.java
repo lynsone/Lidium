39dcf2c921a733ce394355c1504f3bac4978fd61
Windyboy <y765432100@hotmail.com>
2021-06-05 16:09:45 +0800

    fix cashshop server

--- src/server/Start.java
+++ src/server/Start.java
@@ -2,6 +2,7 @@ package server;
 
 import client.SkillFactory;
 import client.inventory.MapleInventoryIdentifier;
+import client.messages.CommandProcessor;
 import constants.ServerConstants;
 import handling.MapleServerHandler;
 import handling.channel.ChannelServer;
@@ -11,6 +12,7 @@ import handling.login.LoginInformationProvider;
 import handling.world.World;
 import java.sql.SQLException;
 import database.DatabaseConnection;
+import handling.cashshop.CashShopServer;
 import handling.world.family.MapleFamily;
 import handling.world.guild.MapleGuild;
 import java.sql.PreparedStatement;
@@ -92,9 +94,9 @@ public class Start {
         ChannelServer.startChannel_Main();
         System.out.println("[Channel Initialized]");
 
-        //System.out.println("[Loading CS]");
-        //CashShopServer.run_startup_configurations();
-        //System.out.println("[CS Initialized]");
+        System.out.println("[Loading CS]");
+        CashShopServer.run_startup_configurations();
+        System.out.println("[CS Initialized]");
         CheatTimer.getInstance().register(AutobanManager.getInstance(), 60000);
         Runtime.getRuntime().addShutdownHook(new Thread(new Shutdown()));
         World.registerRespawn();
@@ -105,6 +107,7 @@ public class Start {
         MapleMonsterInformationProvider.getInstance().addExtra();
         LoginServer.setOn(); //now or later
         System.out.println("[Fully Initialized in " + ((System.currentTimeMillis() - startTime) / 1000) + " seconds]");
+        CommandProcessor.touchClass();
         RankingWorker.run();
     }
 
