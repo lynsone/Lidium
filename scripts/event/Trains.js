73040a9935d052fb3c0e8a80b791d349380b8c80
Manu <16203442+manuelex20@users.noreply.github.com>
2021-06-25 23:00:31 -0500

    Trains.js's event fixed.
    Fix broadcast ship

--- scripts/event/Trains.js
+++ scripts/event/Trains.js
@@ -1,3 +1,8 @@
+var ROAD_TO_LUDI = 200090100;
+var ROAD_TO_ORBIS = 200090110;
+var LUDI_STATION = 220000110;
+var ORBIS_STATION_TO_LUDI = 200000121;
+
 function init() {
     scheduleNew();
 }
@@ -10,23 +15,22 @@ function scheduleNew() {
 }
 
 function stopEntry() {
-    em.setProperty("entry","false");
+    em.setProperty("entry", "false");
 }
 
 function takeoff() {
-    em.warpAllPlayer(200000122, 200090100);
-    em.warpAllPlayer(220000111, 200090110);
-    em.broadcastShip(200000121, 3);
-    em.broadcastShip(220000110, 3);
-    em.setProperty("docked","false");
+    em.warpAllPlayer(200000122, ROAD_TO_LUDI);
+    em.warpAllPlayer(220000111, ROAD_TO_ORBIS);
+    em.broadcastShip(ORBIS_STATION_TO_LUDI, 8, 2);
+    em.broadcastShip(LUDI_STATION, 8, 2);
+    em.setProperty("docked", "false");
     em.schedule("arrived", 420000); //The time that require move to destination
 }
-
 function arrived() {
-    em.warpAllPlayer(200090100, 220000110); // from orbis
-    em.warpAllPlayer(200090110, 200000121); // from ludi
-    em.broadcastShip(200000121, 1);
-    em.broadcastShip(220000110, 1);
+    em.warpAllPlayer(ROAD_TO_LUDI, LUDI_STATION); // from orbis
+    em.warpAllPlayer(ROAD_TO_ORBIS, ORBIS_STATION_TO_LUDI); // from ludi
+    em.broadcastShip(ORBIS_STATION_TO_LUDI, 12, 6);
+    em.broadcastShip(LUDI_STATION, 12, 6);
     scheduleNew();
 }
 
