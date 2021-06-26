1cc44836d633d66932712dd2a8e868f4162f50e3
Manu <16203442+manuelex20@users.noreply.github.com>
2021-06-25 22:56:20 -0500

    NPC Sunny wasn't checking ticket (Ludibrium train function)

--- scripts/npc/2012013.js
+++ scripts/npc/2012013.js
@@ -1,9 +1,12 @@
 /* 
-	NPC Name: 		Sunny
-	Map(s): 		Orbis: Station<To Ludibrium> (200000121)
-	Description: 		Orbis Ticketing Usher
-*/
+ * NPC Name: 		Sunny
+ * NPC ID:              2012013
+ * Map(s): 		Orbis: Station<To Ludibrium> (200000121)
+ * Description: 	Orbis Ticketing Usher
+ */
 var status = 0;
+var STATION_TO_LUDI = 200000121;
+var TICKET_TO_LUDI = 4031074;
 
 function start() {
     status = -1;
@@ -13,26 +16,30 @@ function start() {
 
 function action(mode, type, selection) {
     status++;
-    if(mode == 0) {
-	cm.sendNext("You must have some business to take care of here, right?");
-	cm.dispose();
-	return;
+    if (mode == 0) {
+        cm.sendNext("You must have some business to take care of here, right?");
+        cm.dispose();
+        return;
     }
     if (status == 0) {
-	if(train == null) {
-	    cm.sendNext("The boats are currently down.");
-	    cm.dispose();
-	} else if (train.getProperty("entry").equals("true")) {
-	    cm.sendYesNo("It looks like there's plenty of room for this ride. Please have your ticket ready so I can let you in, The ride will be long, but you'll get to your destination just fine. What do you think? Do you want to get on this ride?");
-	} else if (train.getProperty("entry").equals("false") && train.getProperty("docked").equals("true")) {
-	    cm.sendNext("The train is getting ready for takeoff. I'm sorry, but you'll have to get on the next ride. The ride schedule is available through the usher at the ticketing booth.");
-	    cm.dispose();
-	} else {
-	    cm.sendNext("We will begin boarding 1 minutes before the takeoff. Please be patient and wait for a few minutes. Be aware that the subway will take off on time, and we stop receiving tickets 1 minute before that, so please make sure to be here on time.");
-	    cm.dispose();
-	}
-    } else if(status == 1) {
-	cm.warp(200000122, 0);
-	cm.dispose();
+        if (train == null) {
+            cm.sendNext("The boats are currently down.");
+            cm.dispose();
+        } else if (!cm.haveItem(TICKET_TO_LUDI)) {
+            cm.sendNext("You don't have #i" + TICKET_TO_LUDI + "# #bTicket to Ludibrium (Regular)#k. You can buy one in #bOrbis Station Entrance#k. Bye.");
+            cm.dispose();
+        } else if (train.getProperty("entry").equals("true")) {
+            cm.sendYesNo("It looks like there's plenty of room for this ride. Please have your ticket ready so I can let you in, The ride will be long, but you'll get to your destination just fine. What do you think? Do you want to get on this ride?");
+        } else if (train.getProperty("entry").equals("false") && train.getProperty("docked").equals("true")) {
+            cm.sendNext("The train is getting ready for takeoff. I'm sorry, but you'll have to get on the next ride. The ride schedule is available through the usher at the ticketing booth.");
+            cm.dispose();
+        } else {
+            cm.sendNext("We will begin boarding 1 minutes before the takeoff. Please be patient and wait for a few minutes. Be aware that the subway will take off on time, and we stop receiving tickets 1 minute before that, so please make sure to be here on time.");
+            cm.dispose();
+        }
+    } else if (status == 1) {
+        cm.gainItem(TICKET_TO_LUDI, -1);
+        cm.warp(200000122, 0);
+        cm.dispose();
     }
 }
