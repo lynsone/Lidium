0de301381359d6971eaa06285f0f6f1b78c23ec0
Manu <16203442+manuelex20@users.noreply.github.com>
2021-06-29 19:06:58 -0500

    Load MobSkillFactory in a thread

--- src/server/life/MobSkillFactory.java
+++ src/server/life/MobSkillFactory.java
@@ -33,6 +33,7 @@ import java.sql.SQLException;
 
 import database.DatabaseConnection;
 import java.util.List;
+import server.Start;
 import tools.Pair;
 
 public class MobSkillFactory {
@@ -41,7 +42,7 @@ public class MobSkillFactory {
     private static final MobSkillFactory instance = new MobSkillFactory();
 
     public MobSkillFactory() {
-	initialize();
+        initialize();
     }
 
     public static MobSkillFactory getInstance() {
@@ -49,55 +50,60 @@ public class MobSkillFactory {
     }
 
     public static MobSkill getMobSkill(int skillId, int level) {
-	return instance.mobSkillCache.get(new Pair<Integer, Integer>(Integer.valueOf(skillId), Integer.valueOf(level)));
+        return instance.mobSkillCache.get(new Pair<Integer, Integer>(Integer.valueOf(skillId), Integer.valueOf(level)));
     }
 
     private void initialize() {
-        try {
-            Connection con = DatabaseConnection.getConnection();
-            PreparedStatement ps = con.prepareStatement("SELECT * FROM wz_mobskilldata");
-            ResultSet rs = ps.executeQuery();
-            while (rs.next()) {
-                mobSkillCache.put(new Pair<Integer, Integer>(rs.getInt("skillid"), rs.getInt("level")), get(rs));
+        Thread t = new Thread(() -> {
+            long start = System.currentTimeMillis();
+            try {
+                Connection con = DatabaseConnection.getConnection();
+                PreparedStatement ps = con.prepareStatement("SELECT * FROM wz_mobskilldata");
+                ResultSet rs = ps.executeQuery();
+                while (rs.next()) {
+                    mobSkillCache.put(new Pair<Integer, Integer>(rs.getInt("skillid"), rs.getInt("level")), get(rs));
+                }
+                rs.close();
+                ps.close();
+            } catch (Exception e) {
+                e.printStackTrace();
             }
-            rs.close();
-            ps.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+            System.out.println("MOB SKILL Factory loaded in " + (System.currentTimeMillis() - start) + "ms.");
+        });
+        Start.threads.add(t);
     }
 
     private MobSkill get(ResultSet rs) throws SQLException {
         List<Integer> toSummon = new ArrayList<Integer>();
-	final String[] summs = rs.getString("summons").split(", ");
-	if (summs.length <= 0 && rs.getString("summons").length() > 0) {
-	    toSummon.add(Integer.parseInt(rs.getString("summons")));
-	}
+        final String[] summs = rs.getString("summons").split(", ");
+        if (summs.length <= 0 && rs.getString("summons").length() > 0) {
+            toSummon.add(Integer.parseInt(rs.getString("summons")));
+        }
         for (String s : summs) {
-	    if (s.length() > 0 ){
-            	toSummon.add(Integer.parseInt(s));
-	    }
+            if (s.length() > 0) {
+                toSummon.add(Integer.parseInt(s));
+            }
         }
         Point lt = null;
         Point rb = null;
-	//make sure the points exist before adding it
+        //make sure the points exist before adding it
         if (rs.getInt("ltx") != 0 || rs.getInt("lty") != 0 || rs.getInt("rbx") != 0 || rs.getInt("rby") != 0) {
             lt = new Point(rs.getInt("ltx"), rs.getInt("lty"));
             rb = new Point(rs.getInt("rbx"), rs.getInt("rby"));
         }
-	final MobSkill ret = new MobSkill(rs.getInt("skillid"), rs.getInt("level"));
-	ret.addSummons(toSummon);
-	ret.setCoolTime(rs.getInt("interval") * 1000);
-	ret.setDuration(rs.getInt("time") * 1000);
-	ret.setHp(rs.getInt("hp"));
-	ret.setMpCon(rs.getInt("mpcon"));
-	ret.setSpawnEffect(rs.getInt("spawneffect"));
-	ret.setX(rs.getInt("x"));
-	ret.setY(rs.getInt("y"));
-	ret.setProp(rs.getInt("prop") / 100f);
-	ret.setLimit((short)rs.getInt("limit"));
-	ret.setOnce(rs.getByte("once") > 0);
-	ret.setLtRb(lt,rb);
+        final MobSkill ret = new MobSkill(rs.getInt("skillid"), rs.getInt("level"));
+        ret.addSummons(toSummon);
+        ret.setCoolTime(rs.getInt("interval") * 1000);
+        ret.setDuration(rs.getInt("time") * 1000);
+        ret.setHp(rs.getInt("hp"));
+        ret.setMpCon(rs.getInt("mpcon"));
+        ret.setSpawnEffect(rs.getInt("spawneffect"));
+        ret.setX(rs.getInt("x"));
+        ret.setY(rs.getInt("y"));
+        ret.setProp(rs.getInt("prop") / 100f);
+        ret.setLimit((short) rs.getInt("limit"));
+        ret.setOnce(rs.getByte("once") > 0);
+        ret.setLtRb(lt, rb);
         return ret;
     }
 }
