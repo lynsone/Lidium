fede6a4d3d528d803215d5013f7e1fc0d7d2b5ce
Manu <16203442+manuelex20@users.noreply.github.com>
2021-06-29 20:27:28 -0500

    Server laoding with all threads.

--- src/server/Start.java
+++ src/server/Start.java
@@ -15,6 +15,8 @@ import handling.cashshop.CashShopServer;
 import handling.world.family.MapleFamily;
 import handling.world.guild.MapleGuild;
 import java.sql.PreparedStatement;
+import java.util.ArrayList;
+import java.util.List;
 import server.Timer.*;
 import server.events.MapleOxQuizFactory;
 import server.life.MapleLifeFactory;
@@ -29,6 +31,7 @@ public class Start {
     public static long startTime = System.currentTimeMillis();
     public static final Start instance = new Start();
     public static AtomicInteger CompletedLoadingThreads = new AtomicInteger(0);
+    public static List<Thread> threads = new ArrayList<>();
 
     public void run() throws InterruptedException {
         System.setProperty("net.sf.odinms.wzpath", "wz");
@@ -45,9 +48,21 @@ public class Start {
         } catch (SQLException ex) {
             throw new RuntimeException("[EXCEPTION] Please check if the SQL server is active.");
         }
+        System.out.println("Starting " + ServerProperties.getProperty("net.sf.odinms.login.serverName") + " v" + ServerConstants.MAPLE_VERSION + "." + ServerConstants.MAPLE_PATCH);
 
-        System.out.println(ServerProperties.getProperty("net.sf.odinms.login.serverName"));
+        System.out.print(System.lineSeparator());
+
+        long start = System.currentTimeMillis();
+
+        //Loading Skills -> ok
+        SkillFactory.load();
+
+        System.out.print("Loading World... ");
         World.init();
+        System.out.println("loaded in " + (System.currentTimeMillis() - start) + "ms.");
+
+        start = System.currentTimeMillis();
+        System.out.print("Loading Timers... ");
         WorldTimer.getInstance().start();
         EtcTimer.getInstance().start();
         MapTimer.getInstance().start();
@@ -55,57 +70,106 @@ public class Start {
         EventTimer.getInstance().start();
         BuffTimer.getInstance().start();
         PingTimer.getInstance().start();
-        System.out.println("Loading guilds...");
-        MapleGuildRanking.getInstance().load();
-        MapleGuild.loadAll(); //(this); 
-        System.out.println("Loading families...");
-        MapleFamily.loadAll(); //(this); 
-        System.out.println("Loading quest counts...");
-        MapleLifeFactory.loadQuestCounts();
-        MapleQuest.initQuests();
-        System.out.println("Loading etcs...");
-        MapleItemInformationProvider.getInstance().runEtc();
-        System.out.println("Loading mobs...");
-        MapleMonsterInformationProvider.getInstance().load();
-        System.out.println("Loading items...");
-        MapleItemInformationProvider.getInstance().runItems();
-        System.out.println("Loading skills...");
-        SkillFactory.load();
-        System.out.println("Loader 8...");
-        LoginInformationProvider.getInstance();
+        System.out.println("loaded in " + (System.currentTimeMillis() - start) + "ms.");
+
+        //Loading Random Rewards -> ... Should load before Maple Quests. OK
         RandomRewards.load();
-        MapleOxQuizFactory.getInstance();
+
+        //Maple Quest  -> gonna show its own message. ok
+        MapleQuest.initQuests();
+
+        //Load Player NPC
+        PlayerNPC.loadAll();// touch - so we see database problems early... ok
+
+        //Loading Random Rewards -> gonna show its own message. ok
+        MTSStorage.load();
+
+        //Updating Inventory Identifier -> made here bc just a line... ._.
+        Thread t = new Thread(() -> {
+            final long startx = System.currentTimeMillis();
+            MapleInventoryIdentifier.getInstance();
+            System.out.println("Guilds Ranking loaded in " + (System.currentTimeMillis() - startx) + "ms.");
+        });
+        threads.add(t);
+
+        //Loading Guild Ranking -> gonna show its own message. ok
+        MapleGuildRanking.getInstance().load();
+
+        //Loading Guilds -> gonna show its own message. ok
+        MapleGuild.loadAll(); //(this); 
+
+        //Maple Family -> gonna show its own message. ok
+        MapleFamily.loadAll();
+
+        //Loading Maple Carnival Factory ok
         MapleCarnivalFactory.getInstance();
+
+        //Maple Quest count -> gonna show its own message. ok
+        MapleLifeFactory.loadQuestCounts();
+
+        Thread t2 = new Thread(() -> {
+
+            //Load ETC  -> gonna show its own message.
+            MapleItemInformationProvider.getInstance().runEtc();
+
+            //Loading Mobs -> ...
+            MapleMonsterInformationProvider.getInstance().load();
+
+            //Loading Items -> ...
+            MapleItemInformationProvider.getInstance().runItems();
+
+        });
+        threads.add(t2);
+
+        //Loading Login information -> ... ok
+        LoginInformationProvider.getInstance();
+
+        //Loading MapleOxQuizFactory  -> ... ok
+        MapleOxQuizFactory.getInstance();
+
+        //Loading Mob Skill Factory -> ... ok
         MobSkillFactory.getInstance();
+
         //SpeedRunner.loadSpeedRuns();
-        //MTSStorage.load();
-        System.out.println("Loader 9...");
-        MapleInventoryIdentifier.getInstance();
-        //System.out.println("Loader 10...");
-        CashItemFactory.getInstance().initialize();  
+        //Loading Cash Item Factory -> ... ok
+        CashItemFactory.getInstance().initialize();
+
+        //Loading MapleServerHandler -> ... ok
         MapleServerHandler.initiate();
-        System.out.println("[Loading Login]");
+
+        //Loading Login Server... ok
         LoginServer.run_startup_configurations();
-        System.out.println("[Login Initialized]");
 
-        System.out.println("[Loading Channel]");
+        //Start Channel Server... ok
         ChannelServer.startChannel_Main();
-        System.out.println("[Channel Initialized]");
 
-        System.out.println("[Loading CS]");
+        //Load Cash Shop Server -> ...
         CashShopServer.run_startup_configurations();
-        System.out.println("[CS Initialized]");
-        CheatTimer.getInstance().register(AutobanManager.getInstance(), 60000);
-        Runtime.getRuntime().addShutdownHook(new Thread(new Shutdown()));
-        World.registerRespawn();
+
+        Thread t3 = new Thread(() -> {
+            //Loading Cheat Timer - alredy in a thread
+            CheatTimer.getInstance().register(AutobanManager.getInstance(), 60000);
+
+            //Loading Shutdown hook - already in a thread
+            Runtime.getRuntime().addShutdownHook(new Thread(new Shutdown()));
+
+            //Loading respawn - Already in its own thread by channel
+            World.registerRespawn();
+        });
+        threads.add(t3);
+
         //ChannelServer.getInstance(1).getMapFactory().getMap(910000000).spawnRandDrop(); //start it off
+        //Loading ShudownServer Mbean register
         ShutdownServer.registerMBean();
         //ServerConstants.registerMBean();
-        PlayerNPC.loadAll();// touch - so we see database problems early...
+
         MapleMonsterInformationProvider.getInstance().addExtra();
         LoginServer.setOn(); //now or later
-        System.out.println("[Fully Initialized in " + ((System.currentTimeMillis() - startTime) / 1000) + " seconds]");
         RankingWorker.run();
+
+        threads.parallelStream().forEach(tx -> {
+            tx.start();
+        });
     }
 
     public static class Shutdown implements Runnable {
