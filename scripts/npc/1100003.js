306c9d02701025531aec48f0fd57393ddb5d9b30
bcolby45 <bcolby7@gmail.com>
2021-07-17 23:31:23 -0400

    Update 1100003.js

--- scripts/npc/1100003.js
+++ scripts/npc/1100003.js
@@ -1,3 +1,4 @@
+var menu = new Array("Victoria Island");
 var method;
 
 function start() {
@@ -22,13 +23,13 @@ function action(mode, type, selection) {
         if (status == 0) {
             var display = "";
             for (var i = 0; i < menu.length; i++) {
-                display += "\r\n#L" + i + "##b Victoria Island(1000 mesos)#k";
+                display += "\r\n#L" + i + "##b Victoria Island (1000 mesos)#k";
             }
-            cm.sendSimple("Hmm... The winds are favorable. Are you thinking of leaving ereve and going somwhere else? This ferry sails on to Victoria Island, have you >
+            cm.sendSimple("Hmm... The winds are favorable. Are you thinking of leaving ereve and going somwhere else? This ferry sails to Victoria Island, have you taking care of everything you needed to in Ereve? If you happen to be headed toward #bVictoria Island#k I can take you there. What do you day? Are you going to go to Victoria Island?\r\n" + display);
 
         } else if (status == 1) {
             if (cm.getMeso() < 1000) {
-                cm.sendNext("Hmm... Are you sure you have #b1000#k Mesos? Check your Inventory and make sure you have enough. You must pay the fee or I can't let you g>
+                cm.sendNext("Hmm... Are you sure you have #b1000#k Mesos? Check your Inventory and make sure you have enough. You must pay the fee or I can't let you get on...");
                 cm.dispose();
             } else {
                 cm.gainMeso(-1000);
