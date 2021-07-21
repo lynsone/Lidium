5408c2a0166c454a0eeea1b9f6ccf29ef4d1e825
bcolby45 <bcolby7@gmail.com>
2021-07-21 06:40:17 -0400

    Update MapleEventType.java

--- src/server/events/MapleEventType.java
+++ src/server/events/MapleEventType.java
@@ -23,13 +23,13 @@ package server.events;
 
 public enum MapleEventType {
 
-    // Coconut(new int[]{109080000}), //just using one
-    // CokePlay(new int[]{109080010}), //just using one
-    // Fitness(new int[]{109040000, 109040001, 109040002, 109040003, 109040004}),
-    // OlaOla(new int[]{109030001, 109030002, 109030003}),
+    Coconut(new int[]{109080000}), //just using one
+    CokePlay(new int[]{109080010}), //just using one
+    Fitness(new int[]{109040000, 109040001, 109040002, 109040003, 109040004}),
+    OlaOla(new int[]{109030001, 109030002, 109030003}),
     OxQuiz(new int[]{109020001});
-    // Survival(new int[]{809040000, 809040100}),
-    // Snowball(new int[]{109060000}); //just using one
+    //Survival(new int[]{809040000, 809040100}),
+    //Snowball(new int[]{109060000}); //just using one
     public int[] mapids;
 
     private MapleEventType(int[] mapids) {
