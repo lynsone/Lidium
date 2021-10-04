646468a3f9ae4baa3877e94d4f1337f9ca9b302d
Hello! <bcolby7@gmail.com>
2021-10-04 05:34:50 -0700

    Update MapleCharacter.java

--- src/client/MapleCharacter.java
+++ src/client/MapleCharacter.java
@@ -1797,15 +1797,12 @@ public class MapleCharacter extends AnimatedMapleMapObject implements Serializab
     }
     
     public final void addKillCount() {  
-        return;
-        /* comment out until added to config
         if (JobType.getById(this.getJob()) == JobType.Aran) {
            killCount = killCount + 1; 
         } else {
             killCount = killCount + 1;
             client.getSession().write(CField.testCombo(killCount)); 
         }
-        */
     }
     
     public final void resetKillCount() {
